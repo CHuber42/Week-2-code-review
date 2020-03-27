e8478b4352e3ed1e339cdb3ff5b2baee7207221b
Christopher Huber <CHuber42.work@gmail.com>
2020-03-27 09:09:00 -0700

    add 2nd question; next: reorder 1st question to 3rd, re-write 1st

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
  $("form#survey").submit(function(event) {
    event.preventDefault();
+
+   var q1answer = $("input:radio[name=q1]:checked").val(); 
+   var q2answer = $("input:radio[name=q2]:checked").val();
+   var q3answer = $("input:radio[name=q3]:checked").val();
+   var q4answer = $("input:radio[name=q4]:checked").val();
+   var q5answer = $("input:radio[name=q5]:checked").val();
+   var q6answer = $("input:radio[name=q6]:checked").val();
+   console.log(q1answer, q2answer, q3answer, q4answer, q5answer, q6answer);
  });
 
 });
