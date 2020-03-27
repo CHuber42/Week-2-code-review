ed88357166f1f727c0cb476da6034602f9e3cdda
Christopher Huber <CHuber42.work@gmail.com>
2020-03-27 10:04:55 -0700

    add backend logic for tracking inputs, start work on results computation flow

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,38 @@ $(document).ready(function(){
  $("form#survey").submit(function(event) {
    event.preventDefault();
 
+   var python = 0;
+   var ruby = 0;
+   var csharp = 0;
+   var swift = 0;
+   var javascript = 0;
+   var go = 0;
+   var rust = 0;
+
    var q1answer = $("input:radio[name=q1]:checked").val(); 
    var q2answer = $("input:radio[name=q2]:checked").val();
    var q3answer = $("input:radio[name=q3]:checked").val();
    var q4answer = $("input:radio[name=q4]:checked").val();
    var q5answer = $("input:radio[name=q5]:checked").val();
    var q6answer = $("input:radio[name=q6]:checked").val();
+  
    console.log(q1answer, q2answer, q3answer, q4answer, q5answer, q6answer);
+
+   var resultsArray = [python, ruby, csharp, swift, javascript, go, rust];
+   var resultsCounter = 0;
+   var highestValue = 6;
+   var trueResults = [];
+
+   while (resultsCounter > 3) {
+     for (i = 0; i < resultsArray.length; i++) {
+       if (resultsArray[i] === highestValue && resultsCounter < 3) {
+        trueResults.append(i);
+       }
+
+     }
+     highestValue -= 1;
+   }
+   
  });
 
 });
